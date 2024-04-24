38478d4d242fdab06afa1b58e5adb478cf031df4
Ali Kaan Guven <alikaan.guven@gmail.com>
2024-04-24 17:08:10 +0200

    Checkpoint: Almost there

--- CustomNanoAOD/python/PrecisedxyTable_cfi.py
+++ CustomNanoAOD/python/PrecisedxyTable_cfi.py
@@ -3,10 +3,8 @@ Implementation date: 4/4/2024
 Implemented from RecoTracker/TrackExtrapolator/src/TrackExtrapolator.cc
 """
 import FWCore.ParameterSet.Config as cms
-TrackExtrapolationTable = cms.EDProducer("TrackExtrapolator",
+PrecisedxyTable = cms.EDProducer("PrecisedxyTableProducer",
     pvSrc = cms.InputTag("offlineSlimmedPrimaryVertices"),
     svSrc = cms.InputTag("IVFSecondaryVerticesSoftDV"),
-    tkSrc = cms.InputTag("TrackFilter", "seed"),
-    extrapTkOut = cms.string("SDVTkExtra"),
-    extrapSvOut = cms.string("SDVSvExtra")
+    extraOut = cms.string("SDVExtra")
 )
