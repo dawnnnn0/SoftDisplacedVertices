87eb85440c300d095875bc81767b747aea2d1b8a
Ang Li <Ang.Li@oeaw.ac.at>
2024-09-24 11:12:17 +0200

    limit plots

---
+++ Plotter/haddjobs.sh
@@ -0,0 +1,5 @@
+hadd /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/background_2018_hist.root /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/wjetstolnuht*2018_hist.root /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/zjetstonunuht*2018_hist.root /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/qcdht*2018_hist.root
+hadd /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/wjets_2018_hist.root /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/wjetstolnuht*2018_hist.root 
+hadd /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/zjets_2018_hist.root /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/zjetstonunuht*2018_hist.root 
+hadd /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/qcd_2018_hist.root /eos/vbc/group/cms/ang.li/MCHistos_VRCR${1}/qcdht*2018_hist.root 
+hadd /eos/vbc/group/cms/ang.li/DataHistos_VRCR${1}/met2018_hist.root /eos/vbc/group/cms/ang.li/DataHistos_VRCR${1}/met2018*_hist.root
